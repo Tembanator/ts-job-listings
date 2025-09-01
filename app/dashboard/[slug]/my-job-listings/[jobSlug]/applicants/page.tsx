795360dc20f828af34ad33daea0c7632a7f5fd19
Themba Dlamini <49640058+Tembanator@users.noreply.github.com>
2025-09-01 17:34:26 +0200

    Update page.tsx

--- app/dashboard/[slug]/my-job-listings/[jobSlug]/applicants/page.tsx
+++ app/dashboard/[slug]/my-job-listings/[jobSlug]/applicants/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default async function page({
   params,
 }: {
-  params: { jobSlug: string };
+  params: { jobSlug: string; slug: string };
 }) {
   const { jobSlug } = params;
   const applications = await getJobApplications({ job: jobSlug.toString() });
